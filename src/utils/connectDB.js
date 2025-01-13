a5ebe073a34864bb23fd3b77d5e47ca01dca3fcc
Lokesh_Bijarniya_ <152880035+Lokesh-Bijarniya@users.noreply.github.com>
2025-01-13 15:43:53 +0530

    Update package before version bump

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -1,10 +1,32 @@
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('MongoDB Connected');
+//   } catch (err) {
+//     console.error('Failed to connect to MongoDB', err.message);
+//     process.exit(1); // Exit process with failure
+//   }
+// };
+
+// export default connectDB;
+
+
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-const connectDB = async () => {
+const connectDB = async (dbURI) => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    const mongoURI = dbURI || process.env.MONGODB_URI || 'mongodb://localhost:27017/defaultDB'; // Use provided DB URI or fallback to env variable or local DB
+    await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -16,3 +38,4 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
